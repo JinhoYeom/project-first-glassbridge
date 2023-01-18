419f0a4b0c3232af38c7d81b1de49a5764fa06f6
Jinho Yeom <yeomjh2000@gmail.com>
2023-01-18 19:17:12 +0900

    유리 다리 건너기
    0.1.1. 시작화면 보완
    	  	challanger 화면 제작 중

--- src/com/greedy/game/run/MainApplication.java
+++ src/com/greedy/game/run/MainApplication.java
@@ -3,32 +3,35 @@ package com.greedy.game.run;
 import java.util.Scanner;
 
 public class MainApplication {
-// 시작 화면
+	// 시작 화면
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		Record record = new Record();
-		
+		Challanger chger = new Challanger();
+
 		while(true) {
-		System.out.println("========= 메인 메뉴 ========= ");
-		record.nowBest();
-		System.out.println("1. 도전");
-		System.out.println("2. 종료");
-		System.out.println("메뉴화면 선택 : ");
-		int no  = sc.nextInt();
-		
-		switch(no) {
-		case 1 :
+			System.out.println("========= 메인 메뉴 ========= ");
+			record.nowBest();
+			System.out.println("1. 도전");
+			System.out.println("2. 종료");
+			System.out.println("메뉴화면 선택 : ");
+			int no  = sc.nextInt();
+
+			switch(no) {
+			case 1 : System.out.print ("도전자의 이름은?");
+			String name = sc.next();
+			chger.crossBridge(name);
 			break;
-		case 2 : System.out.println("게임을 종료합니다."); 
+			case 2 : System.out.println("게임을 종료합니다."); 
 			return;
-		default : System.out.println("정확한 숫자를 입력해 주세요");
-		break;
+			default : System.out.println("정확한 숫자를 입력해 주세요");
+			break;
+			}
+
+
 		}
-		
-		
-		}
-		
+
 	}
 
 }
